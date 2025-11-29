50dd2e52be77094d97290d5b555c8468ca6a3a00
ujjwal7014 <ujjwal7014@gmail.com>
2025-11-29 13:34:43 +0530

    style(resume): update global styles to include new resume background for improved aesthetics

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light bg-black">
+    <html lang="en" className="resume-bg">
       <body
         className={`${hostGrotesk.variable} ${serif.variable} ${jetbrains.variable}  antialiased`}
       >
