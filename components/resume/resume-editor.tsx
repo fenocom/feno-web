1bc968280a5b615a50107df83212aa2311a361bb
Rahul Biswakaram <rahul.id39@gmail.com>
2025-12-06 13:43:03 +0530

    refactor: improve code formatting and mark unused props in resume editor.

--- components/resume/resume-editor.tsx
+++ components/resume/resume-editor.tsx
@@ -4,37 +4,38 @@ import "./styles/resume-editor.css";
 
 import type { Editor, Extensions } from "@tiptap/core";
 import { EditorContent, useEditor } from "@tiptap/react";
+import { forwardRef, useImperativeHandle } from "react";
 import { extensionsConfig } from "./extensions/config";
 import BubbleMenuGlobal from "./menus/bubble-menu";
 import { template as classicTemplate } from "./templates/classic/template";
-import { forwardRef, useImperativeHandle } from "react";
 
 export type ResumeEditorRef = {
     editor: Editor | null;
 };
 
-export const ResumeEditor = forwardRef<ResumeEditorRef, unknown>((props, ref) => {
+export const ResumeEditor = forwardRef<ResumeEditorRef, unknown>(
+    (_props, ref) => {
+        const editor = useEditor({
+            content: classicTemplate,
+            immediatelyRender: false,
+            extensions: extensionsConfig as Extensions,
+        });
 
-    const editor = useEditor({
-        content: classicTemplate,
-        immediatelyRender: false,
-        extensions: extensionsConfig as Extensions,
-    });
+        useImperativeHandle(ref, () => ({
+            editor: editor,
+        }));
 
-    useImperativeHandle(ref, () => ({
-        editor: editor,
-    }));
-
-    return (
-        <>
-            {editor && <BubbleMenuGlobal editor={editor} />}
-            <div className="resume-page-export" id="resume-print-root">
-                <EditorContent
-                    id="resume-container"
-                    className="w-[210mm] p-0 min-h-[297mm] bg-white outline-none! border-0! box-border nodrag"
-                    editor={editor}
-                />
-            </div>
-        </>
-    );
-});
+        return (
+            <>
+                {editor && <BubbleMenuGlobal editor={editor} />}
+                <div className="resume-page-export" id="resume-print-root">
+                    <EditorContent
+                        id="resume-container"
+                        className="w-[210mm] p-0 min-h-[297mm] bg-white outline-none! border-0! box-border nodrag"
+                        editor={editor}
+                    />
+                </div>
+            </>
+        );
+    },
+);
