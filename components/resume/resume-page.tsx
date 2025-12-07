bbcb3c63fbd77fd00b1231d5b58a0d52715d55c7
Rahul Biswakaram <rahul.id39@gmail.com>
2025-12-07 17:55:52 +0530

    style: adjust padding in resume page and implement CSS variable for page margin

--- components/resume/resume-page.tsx
+++ components/resume/resume-page.tsx
@@ -17,7 +17,7 @@ export const ResumePage = () => {
     };
 
     return (
-        <div className="relative w-full min-h-screen flex items-center justify-center px-10 py-20">
+        <div className="relative w-full min-h-screen flex items-center justify-center px-10 py-4">
             <DottedBackground />
             <div className="p-1 border border-black/5 relative bg-black/5 rounded-xl z-10 backdrop-blur-sm transition-colors duration-300 focus-within:bg-blue-500/5 focus-within:border-blue-500/20 max-h-[90vh] overflow-y-auto overflow-x-hidden">
                 <div className="rounded-lg border border-black/5">
