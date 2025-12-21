38dd13808fb94b4db7396f0171236dc611d901ef
Rahul Biswakarma <rahul.id39@gmail.com>
2025-12-21 19:32:18 +0530

    chore: update toolbar design

--- components/resume/components/menus/toolbar.tsx
+++ components/resume/components/menus/toolbar.tsx
@@ -5,59 +5,59 @@ import { motion } from "framer-motion";
 import { PortfolioButton } from "../../components/portfolio-button";
 
 interface ToolbarProps {
-    onExport?: () => void;
+        onExport?: () => void;
 }
 
 export default function Toolbar({ onExport }: ToolbarProps) {
-    return (
-        <motion.div
-            initial={{ y: 40, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.25 }}
-            className="fixed bottom-6 left-1/2 -translate-x-1/2 flex items-center shadow-xl z-[999] rounded-4xl backdrop-blur-sm backdrop-invert-10 backdrop-grayscale bg-black/5"
-        >
-            <div className="relative z-10 mix-blend-difference text-white">
-                <div className="flex gap-2 items-center px-3 py-2 border border-white/10 rounded-4xl">
-                    <AiIcon size={28} />
-                    <Separator
-                        orientation="vertical"
-                        className="h-6 bg-white/20"
-                    />
-                    <div className="flex gap-1 items-center">
-                        <Button
-                            isIconOnly
-                            size="sm"
-                            variant="ghost"
-                            className="p-1 min-w-8 h-8 rounded-md text-white hover:bg-white/10"
-                        >
-                            <IconPalette size={18} />
-                        </Button>
-                        <Button
-                            isIconOnly
-                            size="sm"
-                            variant="ghost"
-                            onPress={onExport}
-                            className="p-1 min-w-8 h-8 rounded-md text-white hover:bg-white/10"
-                        >
-                            <IconDownload size={18} />
-                        </Button>
-                        <Button
-                            isIconOnly
-                            size="sm"
-                            variant="ghost"
-                            className="p-1 min-w-8 h-8 rounded-md text-white hover:bg-white/10"
-                        >
-                            <IconSettings size={18} />
-                        </Button>
-                    </div>
-                    <Separator
-                        orientation="vertical"
-                        className="h-6 bg-white/20"
-                    />
-                    <PortfolioButton />
-                </div>
-            </div>
-            <div className="top-0 left-0 absolute pointer-events-none w-full h-full bg-[url('/noise.png')] bg-repeat bg-size-[50px] rounded-4xl opacity-50" />
-        </motion.div>
-    );
+        return (
+                <motion.div
+                        initial={{ y: 40, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{ duration: 0.25 }}
+                        className="fixed bottom-6 left-1/2 -translate-x-1/2 flex items-center shadow-xl z-999 rounded-4xl bg-white"
+                >
+                        <div className="relative z-10 text-black">
+                                <div className="flex gap-2 items-center px-3 py-2 border border-black/10 rounded-4xl">
+                                        <AiIcon size={28} />
+                                        <Separator
+                                                orientation="vertical"
+                                                className="h-6"
+                                        />
+                                        <div className="flex gap-1 items-center">
+                                                <Button
+                                                        isIconOnly
+                                                        size="sm"
+                                                        variant="ghost"
+                                                        className="p-1 min-w-8 h-8 rounded-md text-black hover:bg-black/10"
+                                                >
+                                                        <IconPalette size={18} />
+                                                </Button>
+                                                <Button
+                                                        isIconOnly
+                                                        size="sm"
+                                                        variant="ghost"
+                                                        onPress={onExport}
+                                                        className="p-1 min-w-8 h-8 rounded-md text-black hover:bg-black/10"
+                                                >
+                                                        <IconDownload size={18} />
+                                                </Button>
+                                                <Button
+                                                        isIconOnly
+                                                        size="sm"
+                                                        variant="ghost"
+                                                        className="p-1 min-w-8 h-8 rounded-md text-black hover:bg-black/10"
+                                                >
+                                                        <IconSettings size={18} />
+                                                </Button>
+                                        </div>
+                                        <Separator
+                                                orientation="vertical"
+                                                className="h-6"
+                                        />
+                                        <PortfolioButton />
+                                </div>
+                        </div>
+                        <div className="top-0 left-0 absolute pointer-events-none w-full h-full bg-[url('/noise.png')] bg-repeat bg-size-[50px] rounded-4xl opacity-50" />
+                </motion.div>
+        );
 }
